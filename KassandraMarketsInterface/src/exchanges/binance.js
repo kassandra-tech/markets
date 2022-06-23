0a2c037bf62fdcd6edec96af160eed4aac1bba02
Mike <mikeblack25@gmail.com>
2022-06-23 07:45:53 -0700

    Add support for writing Binance price data to the Moralis database

--- KassandraMarketsInterface/src/exchanges/binance.js
+++ KassandraMarketsInterface/src/exchanges/binance.js
@@ -1,13 +1,22 @@
-const BinanceInterface = require('binance-api-node');
+const { MarketPrice } = require('../models/marketPrice');
 const { Markets } = require('../models/markets');
+const BinanceInterface = require('binance-api-node');
 const binance = BinanceInterface.default({
     apiKey: process.env.BINANCE_API_KEY,
     apiSecret: process.env.BINANCE_API_SECRET
   });
+const Moralis = require("moralis/node");
+
+const serverUrl = "https://dqywxuqq3yjn.usemoralis.com:2053/server";
+const appId = "QHKl8mDT6Uaw7D6RMJIzgSpiAqksNBLO0OPLECN5";
+
+Moralis.start({ serverUrl, appId });
 
 class Binance {
     constructor() {
         this.name = 'Binance';
+        this.trades = null;
+        this.initializeSockets();
     }
 
     async markets() {
@@ -33,6 +42,32 @@ class Binance {
         }
 
         return market;
+    }
+
+    async initializeSockets() {
+        let markets = Moralis.Object.extend(this.name + "Markets");
+        let query = new Moralis.Query(markets);
+        query.descending("createdAt");
+        let results = await query.first();
+        let data = results.get("markets");
+        let marketArray = [];
+        let updates = {};
+        let updateTime = Date.now();
+
+        // Add all markets to add to trades watch.
+        data.forEach((element) => {
+            marketArray.push(element.name);
+        })
+
+        binance.ws.trades(marketArray, (trades) => {
+            let data = new MarketPrice(this.name, trades);
+            updates[data.symbol] = data;
+
+             if (Date.now() > updateTime + 500) {
+                updateTime = Date.now();
+                data.saveData(updates);
+             }
+        });
     }
 }
 
---
+++ KassandraMarketsInterface/src/models/marketPrice.js
@@ -0,0 +1,40 @@
+const Moralis = require("moralis/node");
+
+class MarketPrice {
+    constructor(exchange, data) {
+        this.exchange = exchange;
+        this.symbol = data.symbol;
+        this.time = data.tradeTime;
+        this.price = data.price;
+    }
+
+    async saveData (prices) {
+        try {
+            let MarketObj = Moralis.Object.extend("Prices");
+            let query = new Moralis.Query(MarketObj);
+            query.equalTo("exchange", this.exchange);
+            query.live = true;
+            let results = await query.find();
+    
+            if (results.length > 0) {
+                let result = results[0];
+                result.set("exchange", this.exchange);
+                result.set("prices", prices);
+                result.save();
+            } else {
+                let marketObj = new MarketObj();
+    
+                marketObj.set("exchange", this.exchange);
+                marketObj.set("prices", prices);
+                marketObj.save();
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = {
+    MarketPrice
+};
